1c18c26f365a25d410d1a4a8d05dec7435910462
benitez881 <pashakostrov.pk@gmail.com>
2022-11-11 17:35:20 +0200

    fix: change api for map request

--- components/LocationText/LocationText.tsx
+++ components/LocationText/LocationText.tsx
@@ -7,34 +7,34 @@ type Props = {
   long: string;
 };
 type Location = {
-  country: string;
-  name: string;
+  adminArea1: string;
+  adminArea5: string;
 };
 
 const LocationText = ({ lat, long }: Props) => {
   const [result, setResult] = useState<Location>({
-    country: "",
-    name: "",
+    adminArea1: "",
+    adminArea5: "",
   });
   useEffect(() => {
     (async function () {
       const data = await fetch(
-        `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&appid=36903dad0180d4dfea887b137bca0a8a`
+        `https://www.mapquestapi.com/geocoding/v1/reverse?key=d53It9Y3Ante9kOY86E91VIZqIhj73Zz&location=${lat},${long}`
       );
-      // `https://www.mapquestapi.com/geocoding/v1/reverse?key=d53It9Y3Ante9kOY86E91VIZqIhj73Zz&location=${lat},${long}``;
+      // `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&appid=36903dad0180d4dfea887b137bca0a8a`
       const json = await data.json();
-      const results = await json[0];
-      if (!results?.country && !results?.name) return;
-      const { country, name } = results;
-      setResult({ country, name });
+      const { adminArea1, adminArea5 } = await json.results[0].locations[0];
+
+      if (!adminArea1 && !adminArea5) return;
+      setResult({ adminArea1, adminArea5 });
     })();
   }, [lat, long]);
 
-  const { country, name } = result;
-  return country && name ? (
+  const { adminArea1, adminArea5 } = result;
+  return adminArea1 && adminArea5 ? (
     <div className={styles.location__container}>
       <PlaceIcon viewBox="5 2 14 20" className={styles.image} />
-      {name}, {country}
+      {adminArea1}, {adminArea5}
     </div>
   ) : (
     <></>
