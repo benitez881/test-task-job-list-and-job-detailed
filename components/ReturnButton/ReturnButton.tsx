ae34d3bde6f54d1cfcbd55376422a2459b723336
benitez881 <pashakostrov.pk@gmail.com>
2022-11-13 18:30:25 +0200

    feat: return button right work

--- components/ReturnButton/ReturnButton.tsx
+++ components/ReturnButton/ReturnButton.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import React from "react";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import styles from "./ReturnButton.module.scss";
+import { useRouter } from "next/router";
 
 const ReturnButton = () => {
+  const router = useRouter();
   return (
-    <Link href={"/jobs/1"} className={styles.return__button}>
+    <div onClick={() => router.back()} className={styles.return__button}>
       <ArrowBackIosIcon />
       <span className={styles.return__text}>RETURN TO JOB BOARD</span>
-    </Link>
+    </div>
   );
 };
 
